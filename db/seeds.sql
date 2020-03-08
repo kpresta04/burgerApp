f62609391bd4fad9bacd28e8323909ada98c9adb
Kellen Presta <kpresta04@gmail.com>
2020-03-08 13:16:50 -0500

    orm schema seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hangover Burger');
+INSERT INTO burgers (name) VALUES ('Avocado Burger');
+INSERT INTO burgers (name) VALUES ('Blue Cheese Burger');
